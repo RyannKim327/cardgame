9dfd55a89f4cf068944ee968f8c70c50c7f3a901
RyannKim327 <rksesgundo123@gmail.com>
2026-03-31 17:02:32 +0800

    03-31-26 17:02 [Transition]

--- src/components/navigator.svelte
+++ src/components/navigator.svelte
@@ -5,7 +5,7 @@
 
 <li
 	on:click={click}
-	class={`${isCurrent ? "bg-sky-800/50 text-sky-500 border-sky-500" : "bg-blue-950/50 text-blue-800 border-blue-800"} cursor-pointer border-2 border-solid rounded p-2 select-none`}
+	class={`${isCurrent ? "bg-sky-800/50 text-sky-500 border-sky-500" : "bg-blue-950/50 text-blue-800 border-blue-800"} transition delay-150 cursor-pointer border-2 border-solid rounded p-2 select-none`}
 >
 	<slot />
 </li>
