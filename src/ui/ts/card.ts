3f89db407fde638be03ddad94ed3cde33848ab57
RyannKim327 <rksesgundo123@gmail.com>
2026-03-31 14:22:24 +0800

    03-31-26 14:22

--- src/ui/ts/card.ts
+++ src/ui/ts/card.ts
@@ -7,15 +7,14 @@ export function CreateCard(n: number) {
 	const img = new Image();
 
 	img.src = imgs[n];
-	img.onload = () => console.log("Card image loaded:", image);
+	img.onload = () => console.log("Card image loaded:", imgs[n]);
 
 	return (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-		if (!img.complete) return; // skip if not loaded yet
+		if (!img.complete) return;
 
 		// Draw image as background
 		ctx.drawImage(img, x, y, w, h);
 
-		// Optional: draw border
 		ctx.strokeStyle = "black";
 		ctx.lineWidth = 2;
 		ctx.strokeRect(x, y, w, h);
